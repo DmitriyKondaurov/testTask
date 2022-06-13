6ba1b8571ed5501c22f5759379efb21861892ebe
Dmytro Kondaurov <invisibl@ukr.net>
2022-06-13 13:09:05 +0300

    add font Montserrat

--- news/src/app/app.component.ts
+++ news/src/app/app.component.ts
@@ -4,7 +4,7 @@ import {RestAPIService} from "./services/rest-api.service";
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.scss']
 })
 
 export class AppComponent {
--- news/src/app/components/home-page/home-page.component.ts
+++ news/src/app/components/home-page/home-page.component.ts
@@ -7,7 +7,7 @@ import {Observable} from "rxjs";
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
-  styleUrls: ['./home-page.component.css']
+  styleUrls: ['./home-page.component.scss']
 })
 export class HomePageComponent {
   cards: Observable<iArticle[]> = this.restAPIService.getArticles(30).pipe(
